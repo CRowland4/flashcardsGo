f941a9dac6bf38c5c363b9b747ebcbc48c6ce84d
Caleb Rowland <cjrowland4@gmail.com>
2024-01-18 07:41:40 -0600

    Commit stage 6 of 7

--- main.go
+++ main.go
@@ -3,145 +3,242 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math/rand"
 	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
+type Card struct {
+	term       string
+	definition string
+	missed     int
+}
+
+const menuPrompt string = "\nInput the action (add, remove, import, export, ask, exit, log, hardest card, reset stats):"
+
+var sessionLog strings.Builder
+
 func main() {
-	cards := new(map[string]string)
-	*cards = make(map[string]string)
+	var cards []Card
 
 	for {
-		switch action := readLine("Input the action (add, remove, import, export, ask, exit):"); action {
+		switch action := readLine(menuPrompt); action {
 		case "add":
-			add(cards)
+			add(&cards)
 		case "remove":
-			remove(cards)
+			remove(&cards)
 		case "import":
-			import_(cards)
+			import_(&cards)
 		case "export":
-			export(*cards)
+			export(cards)
 		case "ask":
-			ask(*cards)
+			ask(&cards)
 		case "exit":
-			fmt.Println("Bye bye!")
+			logPrintln("Bye bye!")
 			return
+		case "log":
+			log_()
+		case "hardest card":
+			hardestCard(cards)
+		case "reset stats":
+			resetStats(&cards)
 		default:
-			fmt.Printf("Command %s not recognized. Please enter another comand.\n\n", action)
+			msg := fmt.Sprintf("Command %s not recognized. Please enter another command.", action)
+			logPrintln(msg)
 		}
 	}
 }
 
-func ask(cards map[string]string) {
-	if len(cards) == 0 {
-		fmt.Println("No cards yet!")
+func resetStats(cards *[]Card) {
+	for i := range *cards {
+		(*cards)[i].missed = 0
+	}
+
+	logPrintln("Card statistics have been reset")
+	return
+}
+
+func hardestCard(cards []Card) {
+	sort.Slice(cards, func(i, j int) bool {
+		return cards[i].missed > cards[j].missed
+	})
+
+	noCardsMsg := "There are no cards with errors."
+	if len(cards) == 0 || cards[0].missed == 0 {
+		logPrintln(noCardsMsg)
+		return
+	}
+
+	var terms []string
+	for _, card := range cards {
+		if card.missed != cards[0].missed {
+			break
+		}
+		terms = append(terms, card.term)
+	}
+
+	if len(terms) == 1 {
+		msg := fmt.Sprintf("The hardest card is \"%s\". You have %d errors answering it.", terms[0], cards[0].missed)
+		logPrintln(msg)
+		return
+	}
+
+	hardestMsg := fmt.Sprintf("The hardest cards are \"%s\"", terms[0])
+	for _, term := range terms[1:] {
+		hardestMsg += fmt.Sprintf(", \"%s\"", term)
+	}
+
+	logPrintln(hardestMsg)
+	return
+}
+
+func log_() {
+	fileName := readLine("File name:")
+	file, _ := os.Create(fileName)
+	defer file.Close()
+
+	file.WriteString(sessionLog.String())
+	logPrintln("The log has been saved")
+	return
+}
+
+func ask(cards *[]Card) {
+	if len(*cards) == 0 {
+		logPrintln("No cards yet!")
 	}
 	count := readInt("How many times to ask?")
 	quizCards(cards, count)
 }
 
-func quizCards(cards map[string]string, count int) {
-	terms := make([]string, len(cards))
-	for term := range cards {
-		terms = append(terms, term)
-	}
-
+func quizCards(cards *[]Card, count int) {
 	for i := 0; i < count; i++ {
-		quizTerm := terms[rand.Intn(len(terms))]
-		if quizTerm == "" { // Since the terms array is a slice, after adding elements, it dynamically creates more slots, initialized with an empty sring
-			i--
+		index := i % len(*cards)
+
+		answerPrompt := fmt.Sprintf("Print the definition of \"%s\"", (*cards)[index].term)
+		answer := readLine(answerPrompt)
+
+		if answer == (*cards)[index].definition {
+			logPrintln("Correct!")
 			continue
 		}
 
-		answerPrompt := fmt.Sprintf("Print the definition of \"%s\"", quizTerm)
-		answer := readLine(answerPrompt)
-
-		if answer == cards[quizTerm] {
-			fmt.Println("Correct!")
-			continue
-		} else if definitionExists(answer, cards) {
-			val2 := getTermFor(answer, cards)
-			fmt.Printf("Wrong. The right answer is \"%s\", but your definition is correct for \"%s\".\n", cards[quizTerm], val2)
+		(*cards)[index].missed++
+		if definitionExists(answer, *cards) {
+			val2 := getTermFor(answer, *cards)
+			format := "Wrong. The right answer is \"%s\", but your definition is correct for \"%s\"."
+			msg := fmt.Sprintf(format, (*cards)[index].definition, val2)
+			logPrintln(msg)
 		} else {
-			fmt.Printf("Wrong. The right answer is \"%s\".\n", cards[quizTerm])
+			msg := fmt.Sprintf("Wrong. The right answer is \"%s\".", (*cards)[index].definition)
+			logPrintln(msg)
 		}
 	}
 	return
 }
 
-func export(cards map[string]string) {
+func export(cards []Card) {
 	fileName := readLine("File name:")
 	file, _ := os.Create(fileName)
 
-	for term, definition := range cards {
-		fmt.Fprintln(file, term, definition)
+	for _, card := range cards {
+		fmt.Fprintln(file, card.term, card.definition, card.missed)
 	}
 
-	fmt.Printf("%d cards have been saved.\n", len(cards))
+	msg := fmt.Sprintf("%d cards have been saved.", len(cards))
+	logPrintln(msg)
 	return
 }
 
-func import_(cards *map[string]string) {
+func import_(cards *[]Card) {
 	fileName := readLine("File name:")
-	if /*file*/ _, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY, 0644); err != nil {
-		fmt.Println("File not found")
+	file, err := os.Open(fileName)
+	if err != nil {
+		logPrintln("File not found")
 		return
 	}
-	// TODO Still have to implement, tests don't specify file format
-	fmt.Printf("%d cards have been loaded.\n", len(*cards)) // TODO should print the amount imoprted, not len(map)
+	defer file.Close()
+	var loadedCards int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		cardString := scanner.Text()
+		addImportCard(cards, cardString)
+		loadedCards++
+	}
+
+	msg := fmt.Sprintf("%d cards have been loaded.", loadedCards)
+	logPrintln(msg)
 	return
 }
 
-func remove(cards *map[string]string) {
-	card := readLine("Which card?")
-	if _, ok := (*cards)[card]; ok {
-		delete(*cards, card)
-		fmt.Println("The card has been removed.")
+func addImportCard(cards *[]Card, cardText string) {
+	var importCard Card
+	fmt.Sscanf(cardText, "%s %s %d", &(importCard.term), &(importCard.definition), &(importCard.missed))
+
+	if !termExists(importCard.term, *cards) {
+		*cards = append(*cards, importCard)
+		return
+	}
+
+	for i, card := range *cards {
+		if importCard.term == card.term {
+			(*cards)[i] = importCard
+			break
+		}
+	}
+
+	return
+}
+
+func remove(cards *[]Card) {
+	cardToDelete := readLine("Which card?")
+	var newCards []Card
+
+	for _, card := range *cards {
+		if card.term != cardToDelete {
+			newCards = append(newCards, card)
+		}
+	}
+
+	if len(*cards) == len(newCards) {
+		msg := fmt.Sprintf("Can't remove \"%s\": there is no such card.", cardToDelete)
+		logPrintln(msg)
 	} else {
-		fmt.Printf("Can't remove \"%s\": there is no such card.\n", card)
+		logPrintln("The card has been removed.")
+		*cards = newCards
 	}
 
 	return
 }
 
-func add(cards *map[string]string) {
-	term := getNewTerm(cards)
-	definition := getNewDefinition(*cards)
-	(*cards)[term] = definition
+func add(cards *[]Card) {
+	newCard := Card{
+		term:       getNewTerm(*cards),
+		definition: getNewDefinition(*cards),
+		missed:     0,
+	}
+	*cards = append(*cards, newCard)
 
-	fmt.Printf("The pair (\"%s\":\"%s\") has been added\n", term, definition)
+	msg := fmt.Sprintf("The pair (\"%s\":\"%s\") has been added", newCard.term, newCard.definition)
+	logPrintln(msg)
 	return
 }
 
-func getNewTerm(cards *map[string]string) (term string) {
-	term = readLine("The card")
-
+func getNewTerm(cards []Card) (newTerm string) {
+	newTerm = readLine("The card")
 	for {
-		if _, ok := (*cards)[term]; !ok {
-			return term
+		if !termExists(newTerm, cards) {
+			return newTerm
 		}
-
-		newPrompt := fmt.Sprintf("The term \"%s\" already exists. Try again:", term)
-		term = readLine(newPrompt)
+		newPrompt := fmt.Sprintf("The term \"%s\" already exists. Try again:", newTerm)
+		newTerm = readLine(newPrompt)
 	}
 }
 
-func getNewDefinition(cards map[string]string) (definition string) {
-	definition = readLine("The definition of the card")
-
-	for {
-		if !definitionExists(definition, cards) {
-			return definition
-		}
-
-		newPrompt := fmt.Sprintf("The definition \"%s\" already exists. Try again:", definition)
-		definition = readLine(newPrompt)
-	}
-}
-
-func definitionExists(definition string, cards map[string]string) (exists bool) {
-	for _, val := range cards {
-		if definition == val {
+func termExists(term string, cards []Card) (exists bool) {
+	for _, card := range cards {
+		if card.term == term {
 			return true
 		}
 	}
@@ -149,10 +246,33 @@ func definitionExists(definition string, cards map[string]string) (exists bool) 
 	return false
 }
 
-func getTermFor(definition string, cards map[string]string) (term string) {
-	for key, val := range cards {
-		if val == definition {
-			term = key
+func getNewDefinition(cards []Card) (newDefinition string) {
+	newDefinition = readLine("The definition of the card")
+
+	for {
+		if !definitionExists(newDefinition, cards) {
+			return newDefinition
+		}
+
+		newPrompt := fmt.Sprintf("The definition \"%s\" already exists. Try again:", newDefinition)
+		newDefinition = readLine(newPrompt)
+	}
+}
+
+func definitionExists(definition string, cards []Card) (exists bool) {
+	for _, card := range cards {
+		if card.definition == definition {
+			return true
+		}
+	}
+
+	return false
+}
+
+func getTermFor(definition string, cards []Card) (term string) {
+	for _, card := range cards {
+		if card.definition == definition {
+			term = card.term
 			break
 		}
 	}
@@ -161,14 +281,26 @@ func getTermFor(definition string, cards map[string]string) (term string) {
 }
 
 func readLine(prompt string) (line string) {
-	fmt.Println(prompt)
+	logPrintln(prompt)
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
-	return scanner.Text()
+
+	line = scanner.Text()
+	sessionLog.WriteString(line)
+
+	return line
 }
 
 func readInt(prompt string) (num int) {
-	fmt.Println(prompt)
+	logPrintln(prompt)
 	fmt.Scanln(&num)
+
+	sessionLog.WriteString(strconv.Itoa(num))
 	return num
+}
+
+func logPrintln(msg string) {
+	fmt.Println(msg)
+	sessionLog.WriteString(msg)
+	return
 }
